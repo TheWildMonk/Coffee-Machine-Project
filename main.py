3e11323088fffe4c5572541276d3e56fab48874a
Shuvo Nasir <shuvonasir028@gmail.com>
2021-07-07 13:08:41 +0600

    add TODOs' for the project

---
+++ main.py
@@ -0,0 +1,17 @@
+# Import necessary libraries
+from data import MENU, resources
+
+# TODO: 1. Ask the user for their choice of either coffee or the report of current status of the coffee machine
+#  or stop the machine.
+
+# TODO: 2. If user asks for the report, provide the current status of the report.
+#  Else ask user for coins.
+
+# TODO: 2a. If there's not enough resource, let user know and ask the user again.
+#  Else, ask the user for coins.
+
+
+# TODO: 2b. If user didn't provided enough money, let the user know and refund the money.
+#  Else, give the change to user and serve the Coffee.
+
+# TODO: 2c. Deduct the resources used from original resources.
