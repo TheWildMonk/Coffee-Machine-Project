cef04d529a8f05c301926d4e9bd4adcd6c28dcff
Shuvo Nasir <3355257+TheWildMonk@users.noreply.github.com>
2024-01-29 12:42:40 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,62 +1,69 @@
 # Import necessary libraries
 from data import MENU, resources
 
-# Creating variables
-water = resources["water"]
-milk = resources["milk"]
-coffee = resources["coffee"]
-money = 0
 
-close_machine = False
-while not close_machine:
+# Function for check whether the coffee machine has enough resource.
+def is_resource_sufficient(coffee_ingredients):
+    """The function takes the amount of ingredients needed for user's coffee and return
+    a boolean value.
+    Not enough resource --> False
+    Enough resource --> True"""
+    for key in coffee_ingredients:
+        if coffee_ingredients[key] > resources[key]:
+            print(f"There is not enough {key}.")
+            return False
+    return True
 
-    # TODO: 1. Ask the user for their choice of either coffee or the report of current status of the coffee machine
-    #  or stop the machine.
-    user_choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
 
-    # TODO: 2. If user asks for the report, provide the current status of the report.
-    #  Else ask user for coins.
-    if user_choice == "off":
-        close_machine = True
-    elif user_choice == "report":
-        print(f"Water: {water}ml\n"
-              f"Milk: {milk}ml\n"
-              f"Coffee: {coffee}g\n"
-              f"Money: ${money}")
-    elif user_choice == "espresso" or user_choice == "latte" or user_choice == "cappuccino":
-
-        # TODO: 2a. If there's not enough resource, let user know and ask the user again.
-        #  Else, ask the user for coins.
-        if water < MENU[user_choice]["ingredients"]["water"] and milk < MENU[user_choice]["ingredients"]["milk"] and \
-                coffee < MENU[user_choice]["ingredients"]["coffee"]:
-            print(f"Sorry. there is not enough water 💧 for {user_choice}.")
-        else:
-            quarters = int(input("How many quarters?: ")) * 0.25
-            dimes = int(input("How many dimes?: ")) * 0.10
-            nickels = int(input("How many nickels?: ")) * 0.05
-            pennies = int(input("How many pennies?: ")) * 0.01
-            user_money = quarters + dimes + nickels + pennies
-            change = user_money - MENU[user_choice]["cost"]
-
-            # TODO: 2b. If user didn't provided enough money, let the user know and refund the money.
-            #  Else, give the change to user and serve the Coffee.
-            if user_money < MENU[user_choice]["cost"]:
-                print("Sorry, that's not enough money. Money refunded.")
-                money = money
-            else:
-                if change != 0:
-                    print(f"Here is ${round(change, 2)} in change.")
-                print(f"Here is your {user_choice} ☕, enjoy")
-
-                # TODO: 2c. Deduct the resources used from original resources.
-                if user_choice != "espresso":
-                    water -= MENU[user_choice]["ingredients"]["water"]
-                    milk -= MENU[user_choice]["ingredients"]["milk"]
-                    coffee -= MENU[user_choice]["ingredients"]["coffee"]
-                    money += MENU[user_choice]["cost"]
-                else:
-                    water -= MENU[user_choice]["ingredients"]["water"]
-                    coffee -= MENU[user_choice]["ingredients"]["coffee"]
-                    money += MENU[user_choice]["cost"]
+# Function to process the user provided coins in the machine.
+def process_coins(coffee, quarter, dime, nickle, penny):
+    """The function takes user's coffee choice along with provided coins for the coffee.
+    If there is not enough coins, the money is refunded. Otherwise, it gives the change to the
+    user and serves the coffee."""
+    global money
+    coin_value = 0.25 * quarter + 0.1 * dime + 0.05 * nickle + 0.01 * penny
+    if coin_value < MENU[coffee]["cost"]:
+        print("Sorry, that's not enough money. Money refunded.")
     else:
-        print("Please enter correct coffee name.")
+        print(f"Here is ${round(coin_value - MENU[coffee]['cost'], 2)} in change.")
+        print(f"Here is your {coffee} ☕. Enjoy!")
+        money += MENU[coffee]['cost']
+        deduct_resources(coffee)
+
+
+# Function to deduct coffee resources after a successful transaction in the coffee machine.
+def deduct_resources(coffee):
+    """The function takes user's coffee choice and deduct the ingredient of the coffee from
+    the machine resources."""
+    for ingredient in MENU[coffee]["ingredients"]:
+        resources[ingredient] = resources[ingredient] - MENU[coffee]["ingredients"][ingredient]
+
+
+# List of coffee choices present in the coffee machine.
+coffee_choices = ["espresso", "latte", "cappuccino"]
+
+# Main coffee machine functionality.
+money = 0.0
+machine_start = True
+while machine_start:
+    # Ask user for either current status of the machine, a coffee choice or t
+    user_choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    if user_choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${money}")
+    elif user_choice in coffee_choices:
+        ingredients = MENU[user_choice]["ingredients"]
+        coffee_cost = MENU[user_choice]["cost"]
+        if is_resource_sufficient(ingredients):
+            print("Please insert coins.")
+            quarters = float(input("How many quarters?: $"))
+            dimes = float(input("How many dimes?: $"))
+            nickles = float(input("How many nickles?: $"))
+            pennies = float(input("How many pennies?: $"))
+            process_coins(user_choice, quarters, dimes, nickles, pennies)
+    elif user_choice == "off":
+        machine_start = False
+    else:
+        print("Wrong choice, please try again!")
